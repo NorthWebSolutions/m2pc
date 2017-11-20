1bd4ccda42a22aba27eef2a9a9e5b04b33442f55
Gabor Eszaki <gaboreszaki@users.noreply.github.com>
2017-11-20 12:42:38 +0100

    Create correct_permissions.sh

---
+++ correct_permissions.sh
@@ -0,0 +1,18 @@
+#Locate magento
+LIB="/var/www/magento/"
+
+echo "Change username and groupe for all files..."
+#Change magento_user and magento_group to your magento username and group
+sudo chown -R magento_user:magento_group $LIB   
+wait
+echo "correct read-write access"
+sudo chmod -R 770 $LIB
+wait
+cd $LIB
+echo "Search for specific file types and locations to correct permissions"
+sudo find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} \;
+sudo find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} \;
+echo "Add execute permission for magento"
+sudo chmod u+x bin/magento
+wait
+echo "Process finished."
